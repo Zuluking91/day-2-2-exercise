b73a6613ff9b28073bee88133c98563ff0b02ed0
Zuluking91 <52240454+Zuluking91@users.noreply.github.com>
2022-09-15 13:33:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+# BMI = height / weight** 2
+BMI = int(weight) / float(height) ** 2
+BMI_as_int = int(BMI)
+print(BMI_as_int)
+
+
+
+
+
+
+
+
+
+
+
